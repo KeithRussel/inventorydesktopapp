238a2078603f7fa1c10e4c3ac67706ef9c16decc
KeithRussel <keithmaniacop@gmail.com>
2021-06-09 21:52:54 +0800

    Setup backend server

---
+++ backend/server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const colors = require('colors');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+
+if (process.env.NODE_ENV !== 'production') {
+  app.get('/', (req, res) => {
+    res.send('API is running...');
+  });
+}
+
+const PORT = 5000;
+
+app.listen(
+  PORT,
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
+);
