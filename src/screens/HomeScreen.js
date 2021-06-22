4d2ef140aba0b70a9c55ac04bfa4cde669148d02
KeithRussel <keithmaniacop@gmail.com>
2021-06-22 14:55:57 +0800

    Integrate Table and Pull data from backend

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,26 +1,29 @@
 import server from '../utils/server';
 import useRequest from '../utils/useRequest';
 
-const HomeScreen = () => {
-  const { data: result, error } = useRequest({
-    url: `${server}/api/products`,
-  });
-  // const { data: result, error } = useSWR(`${server}/api/products`);
+import Table from '../components/Table';
 
-  if (error) return <div>Failed to load</div>;
-  if (!result) return <div>loading...</div>;
-  if (result) {
-    console.log(result);
-  }
+const HomeScreen = () => {
+  // const { data: result, error } = useRequest({
+  //   url: `${server}/api/products`,
+  // });
+  // // const { data: result, error } = useSWR(`${server}/api/products`);
+
+  // if (error) return <div>Failed to load</div>;
+  // if (!result) return <div>loading...</div>;
+  // if (result) {
+  //   console.log(result);
+  // }
 
   return (
     <>
       <div>Home Screens</div>
-      <div>
+      {/* <div>
         {result
           ? result.map((item) => <p key={item.prod_id}>{item.name}</p>)
           : null}
-      </div>
+      </div> */}
+      <Table />
     </>
   );
 };
